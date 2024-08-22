045a49de218dfb7863c53623f1d2a554336361bb
AVIRAL1854 <aviralrawat365@gmail.com>
2024-08-22 07:07:10 +0530

    streak-table-implementation

--- frontend/my-app/Components/StreakCard.tsx
+++ frontend/my-app/Components/StreakCard.tsx
@@ -1,13 +1,48 @@
-import React from 'react';
-const StreakCard=()=>{
-    return <div>
-
-        <div>Latest Streak :45 days</div>
-        <div>
-
-            <table></table>
-        </div>
-
-
+import React from "react";
+const StreakCard = ({first,second,third}) => {
+  return (
+    <div className=" w-full h-full mx-20 flex grid border-white border-2 justify-center place-content-center bg-blue-200 rounded-md drop-shadow-2xl">
+      <section className="h-40 font-bold   w-full justify-center  bg-red-500 border-2 border-white rounded-sm  text-center text-3xl">
+        Latest Streak :<span className=" font-bold text-5xl"> 45 days</span>
+      </section>
+      <section className="h-full w-full flex justify-center bg-green-200">
+        <table className="border-2 border-black w-full  flex-rows">
+          <tr className="border-2 border-black  w-full text-xl  md:text-2xl">
+            <th className="flex-rows w-1/2 border-2 border-white rounded-sm border-r-black">
+              Postion
+            </th>
+            <th className="flex-rows w-2/3 border-2 border-white rounded-sm border-l-black">
+              Days
+            </th>
+          </tr>
+          <tr className="border-2 border-black  w-full text-xl  md:text-2xl">
+            <td className="flex-rows w-1/2 border-2 border-white rounded-sm border-r-black">
+              1st{" "}
+            </td>
+            <td className="flex-rows w-2/3 border-2 border-white rounded-sm border-l-black">
+              {first} days
+            </td>
+          </tr>
+          <tr className="border-2 border-black  w-full text-xl  md:text-2xl">
+            <td className="flex-rows w-1/2 border-2 border-white rounded-sm border-r-black">
+              2nd
+            </td>
+            <td className="flex-rows w-2/3 border-2 border-white rounded-sm border-l-black">
+              {second} days
+            </td>
+          </tr>
+          <tr className="border-2 border-black  w-full text-xl  md:text-2xl">
+            <td className="flex-rows w-1/2 border-2 border-white rounded-sm border-r-black">
+              2nd
+            </td>
+            <td className="flex-rows w-2/3 border-2 border-white rounded-sm border-l-black">
+              {third} days
+            </td>
+          </tr>
+        </table>
+      </section>
     </div>
-}
+  );
+};
+
+export default StreakCard;
--- frontend/my-app/app/user-main-profile/page.tsx
+++ frontend/my-app/app/user-main-profile/page.tsx
@@ -3,20 +3,28 @@ import Navbar from "@/Components/Navbar";
 // import Calendar from '@/Components/CalendarCha'
 import CalendarChart from "@/Components/CalendarChart";
 import { ProgressGraph } from "@/Components/ProgressGraph";
+import StreakCard from "@/Components/StreakCard";
 
 const page = () => {
   return (
-    <div className="h-screen   flex-cols w-full text-black bg-violet-500">
+    <div className="h-full  flex-cols w-full text-black bg-violet-500">
       <section className="flex flex-cols w-full">
         <Navbar />
       </section>
       <section className=" flex-rows w-full">
         this is the main profile here
-        <div className="  w-full flex bg-black rounded-xl  drop-shadow-lg ">
-          <CalendarChart />{" "}
+        <div className="  w-full flex bg-white rounded-xl  drop-shadow-lg place-content-center h-40 md:h-min  ">
+          <CalendarChart />
+          {/* <div className="text-white">Streak : 45 days</div> */}
         </div>
-        <div className="w-full flex flex-cols w-screen ">
-          <ProgressGraph />
+        <div className="w-full flex grid lg:flex  my-5 md:my-5 lg:my-10">
+          <div className="flex w-full  bg-black justify-center sm:my-10">
+            {" "}
+            <ProgressGraph />
+          </div>
+          <div className="flex w-full  h-96 bg-white  justify-center my-10 text-center justify-center rounded-lg drop-shadow-2xl ">
+            <StreakCard first={45} second={25} third={10} />
+          </div>
         </div>
       </section>
       <section></section>
