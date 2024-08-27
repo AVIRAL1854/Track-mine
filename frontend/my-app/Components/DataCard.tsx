dacf3679b112be1b2bab8807db2b27a6d43f13bd
AVIRAL1854 <aviralrawat365@gmail.com>
2024-08-27 10:44:55 +0530

    update

--- frontend/my-app/Components/DataCard.tsx
+++ frontend/my-app/Components/DataCard.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 // import CardValueGenerator from './Cardgenerator';
 const DataCard=({time, inputText})=>{
     return (
-      <div className="mx-2 shadow-lg shadow-cyan-500/50 flex-rows bg-black w-84 sm:w-84 justify-center  place-content-center border-black border-2 rounded-3xl my-2 drop-shadow-xl shadow-cyan-500/50 hover:border-2 hover:ring-2 hover:shadow-cyan-500/90 hover:ring-white ">
+      <div className="mx-2 shadow-lg shadow-cyan-500/50 flex-rows bg-black w-84 sm:w-84 justify-center  place-content-center border-black border-2 rounded-3xl my-2 drop-shadow-xl shadow-cyan-500/50 hover:border-2 hover:ring-2 hover:shadow-cyan-500/80  ">
         <section className="flex justify-center text-center text-white w-full text-4xl font-bold bg-gradient-to-r from-black to-slate-900 rounded-3xl">
           {time}
         </section>
